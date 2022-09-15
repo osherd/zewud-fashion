d99a9280120108de36ad6dd62037f8d7f8727922
Osher Demeke <osherdemeke@gmail.com>
2022-09-15 13:06:31 +0300

    add asset and navigation component

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Outlet,Link} from 'react-router-dom';
+
+import {ReactComponent as ZewudLogo} from '../../assets/zewud.svg';
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <>
+      <div className='navigation'>
+        <Link className='logo-container' to ='/'>
+       <ZewudLogo className ='logo'/>
+          </Link>
+        <div className='nav-links-container'>
+          <Link  className='nav-link' to = '/shop'>
+            SHOP
+          </Link>
+            <Link  className='nav-link' to = '/sign-in'>
+            Sign In
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  )
+}
+export default Navigation;
